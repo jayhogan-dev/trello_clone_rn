47049f9cdce89dac448dc3e4fe4e663da1a72f3a
Jay Hogan <jayhogan.dev@gmail.com>
2024-08-22 12:21:45 -0400

    syncing clerk sign up with supabase

--- components/AuthModal.tsx
+++ components/AuthModal.tsx
@@ -1,4 +1,6 @@
+import { useWarmUpBrowser } from '@/hooks/useWarmUpBrowser';
 import { AuthStrategy, ModalType } from '@/types/enums';
+import { useOAuth, useSignIn, useSignUp } from '@clerk/clerk-expo';
 import { Ionicons } from '@expo/vector-icons';
 import { BottomSheetView } from '@gorhom/bottom-sheet';
 import { Text, StyleSheet, TouchableOpacity, Image } from 'react-native';
@@ -31,9 +33,80 @@ const LOGIN_OPTIONS = [
 ];
 
 const AuthModal = ({ authType }: AuthModalProps) => {
-  const onSelectedAuth = async (strategy: AuthStrategy) => {
-    console.log(strategy);
+  useWarmUpBrowser();
+  const { signUp, setActive } = useSignUp();
+  const { signIn } = useSignIn();
+  const { startOAuthFlow: googleAuth } = useOAuth({
+    strategy: AuthStrategy.Google,
+  });
+  const { startOAuthFlow: microsoftAuth } = useOAuth({
+    strategy: AuthStrategy.Microsoft,
+  });
+  const { startOAuthFlow: slackAuth } = useOAuth({
+    strategy: AuthStrategy.Slack,
+  });
+  const { startOAuthFlow: appleAuth } = useOAuth({
+    strategy: AuthStrategy.Apple,
+  });
+
+  const onSelectAuth = async (strategy: AuthStrategy) => {
+    if (!signIn || !signUp) return null;
+
+    const selectedAuth = {
+      [AuthStrategy.Google]: googleAuth,
+      [AuthStrategy.Microsoft]: microsoftAuth,
+      [AuthStrategy.Slack]: slackAuth,
+      [AuthStrategy.Apple]: appleAuth,
+    }[strategy];
+
+    // https://clerk.com/docs/custom-flows/oauth-connections#o-auth-account-transfer-flows
+    // If the user has an account in your application, but does not yet
+    // have an OAuth account connected to it, you can transfer the OAuth
+    // account to the existing user account.
+    const userExistsButNeedsToSignIn =
+      signUp.verifications.externalAccount.status === 'transferable' &&
+      signUp.verifications.externalAccount.error?.code ===
+        'external_account_exists';
+
+    if (userExistsButNeedsToSignIn) {
+      const res = await signIn.create({ transfer: true });
+
+      if (res.status === 'complete') {
+        setActive({
+          session: res.createdSessionId,
+        });
+      }
+    }
+
+    const userNeedsToBeCreated =
+      signIn.firstFactorVerification.status === 'transferable';
+
+    if (userNeedsToBeCreated) {
+      const res = await signUp.create({
+        transfer: true,
+      });
+
+      if (res.status === 'complete') {
+        setActive({
+          session: res.createdSessionId,
+        });
+      }
+    } else {
+      // If the user has an account in your application
+      // and has an OAuth account connected to it, you can sign them in.
+      try {
+        const { createdSessionId, setActive } = await selectedAuth();
+
+        if (createdSessionId) {
+          setActive!({ session: createdSessionId });
+          console.log('OAuth success standard');
+        }
+      } catch (err) {
+        console.error('OAuth error', err);
+      }
+    }
   };
+
   return (
     <BottomSheetView style={styles.modalContainer}>
       <TouchableOpacity style={styles.modalBtn}>
@@ -48,7 +121,7 @@ const AuthModal = ({ authType }: AuthModalProps) => {
         <TouchableOpacity
           key={option.strategy}
           style={styles.modalBtn}
-          onPress={() => onSelectedAuth(option.strategy)}
+          onPress={() => onSelectAuth(option.strategy)}
         >
           <Image source={option.icon} style={styles.btnIcon} />
           <Text style={styles.btnText}>{option.text}</Text>
